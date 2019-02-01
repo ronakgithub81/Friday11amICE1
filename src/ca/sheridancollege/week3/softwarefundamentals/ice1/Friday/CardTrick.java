5b94968c9ed2c0bd14cf5351f300cbf05ac993e9
Bhaldur <bousfiet@sheridancollege.ca>
2019-02-01 12:44:26 -0500

    init commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,15 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Tavin
+ * @ID 991500992
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c1 = new Card();
+       c1.setSuit("hearts");
+       c1.setValue(1);
     }
     
 }

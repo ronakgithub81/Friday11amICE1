1e6a572535501259cf7d8af1048d95cdb3f8569c
androw-99 <47036703+androw-99@users.noreply.github.com>
2019-02-01 13:05:45 -0500

    Added a toString
    Added a toString for Suit and value

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
@@ -43,7 +43,10 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
+   @Override
+    public String toString() {
+        return "Card{" + "suit=" + suit + ", value=" + value + '}';
+    }
    
     
 }

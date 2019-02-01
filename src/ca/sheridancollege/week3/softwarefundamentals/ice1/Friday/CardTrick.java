8e2b9a4694ed82652a128962af4151c8d7c31c8b
romek2 <romannanynets87@gmail.com>
2019-02-01 13:35:54 -0500

    first

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,11 +4,22 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * 
+ * roman nanynets
  */
 public class CardTrick {
     public static void main(String[] args)
     {
+        
+        Card c1 = new Card();
+            
+        c1.setSuit("Hearts");
+        
        
+        c1.setValue(4);
+            
+        
+           
     }
     
 }

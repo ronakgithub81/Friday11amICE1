c9cfe623319fb9e8cd4fa8f69da1a506d8e6b9d2
Bhaldur <bousfiet@sheridancollege.ca>
2019-02-01 13:16:50 -0500

    added Card c2 to remote branch

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -13,6 +13,11 @@ public class CardTrick {
        c1.setSuit("hearts");
        c1.setValue(1);
        
+       Card c2 = new Card();
+       c2.setSuit("spades");
+       c2.setValue(12);
+       
+       
        System.out.println("Suit: " + c1.getSuit() + " Value: "+ c1.getValue());
     }
     

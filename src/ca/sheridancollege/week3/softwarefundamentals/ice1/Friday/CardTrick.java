320ef5f759edbca84398ca98dd00cd38fdeecb64
Kevin <lykevi@sheridancollege.ca>
2019-02-01 13:18:15 -0500

    Kevin Ly Student No: 991514846

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,13 +2,16 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
+ * Kevin Ly 
+ * Student No: 991514846
  * @author Ronak
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+     Card kevinLyCard = new Card();
+     kevinLyCard.setSuit("Hearts");
+     kevinLyCard.setValue(4);
     }
     
 }

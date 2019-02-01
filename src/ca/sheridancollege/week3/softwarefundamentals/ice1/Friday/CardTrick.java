8029d6ec163d9e24606c46ba8fc44b0e6fe29a8a
Rajan Grewal <rajan.grewal@habtech.ca>
2019-02-01 12:53:32 -0500

    Created a second object of the Card class in CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,11 +8,20 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       Card card = new Card();
-       card.setSuit(Card.SUITS[0]);
-       card.setValue(7);
+       Card card1 = new Card();
+       card1.setSuit(Card.SUITS[0]);
+       card1.setValue(6);
         
-       System.out.println("Card Suit: " + card.getSuit());
-       System.out.println("Card Value: " + card.getValue());  
+       System.out.println("Card Suit: " + card1.getSuit());
+       System.out.println("Card Value: " + card1.getValue());  
+       
+       System.out.println();
+       
+       Card card2 = new Card();
+       card2.setSuit(Card.SUITS[3]);
+       card2.setValue(9);
+        
+       System.out.println("Card Suit: " + card2.getSuit());
+       System.out.println("Card Value: " + card2.getValue());  
     }
 }

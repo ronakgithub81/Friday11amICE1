6c077ae4a802211d6b1b6f3cd25b0cbfc5d847fe
Nikolay Rozum <nickrozum@gmail.com>
2019-02-01 12:41:22 -0500

    Nikolay Rozum

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,11 +4,19 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ *
+ * Student ID: 991480365
+ *
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card H9 = new Card();
+
+       H9.setSuit("Hearts");
+       H9.setValue(9);
+
+        System.out.println(H9);
     }
     
 }

2b5e36c6473210bd208b5d673775951ef77cc912
kevly23 <47252344+kevly23@users.noreply.github.com>
2019-02-01 13:28:41 -0500

    updated

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -12,7 +12,7 @@ public class CardTrick {
      Card kevinLyCard = new Card();
      kevinLyCard.setSuit("Hearts");
      kevinLyCard.setValue(4);
-        //printing
+        //printing card
         System.out.println(kevinLyCard.getValue() + kevinLy.getSuit());
     }
     

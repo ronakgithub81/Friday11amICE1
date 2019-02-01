05985800849c2bed20bb44949ed5b309e9de1493
mavrodimmm <fedotovn125@gmail.com>
2019-02-01 12:56:32 -0500

    HI

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,18 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author
+ * 
+ * 
+ * Nikita Fedotov - 991473606
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+    Card one = new Card();
+    one.setSuit("Hearts");
+    one.setValue(5);
+    
     }
     
 }

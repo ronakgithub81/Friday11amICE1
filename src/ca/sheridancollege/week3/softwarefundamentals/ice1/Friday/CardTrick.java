f84c0dc370623d15a95bb70d33d3fd63fcd9fa3d
hill10sheridan <hill10@sheridancollege.ca>
2019-02-01 12:38:31 -0500

    Ryan Hill 991533120

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,13 +2,15 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
- * @author Ronak
+ * Student NO: 991533120
+ * @author Ryan Hill
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card ryanHill = new Card();
+       ryanHill.setSuit("Hearts");
+       ryanHill.setValue(1);
     }
     
 }

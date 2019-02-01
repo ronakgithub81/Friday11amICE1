46259b82395f0f5338d7c799838a6e2dca31fb06
Rajan Grewal <rajan.grewal@habtech.ca>
2019-02-01 12:46:19 -0500

    Created an object of the Card class in CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,14 +1,15 @@
-
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
  * @author Ronak
+ * @author Rajan Grewal 991530913 
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card = new Card();
+       card.setSuit(Card.SUITS[0]);
+       card.setValue(7);
     }
-    
 }

5759427064b109e15ad8aed322a4af942bc1ea3e
Chelsea Penner <pennerch@sheridancollege.ca>
2019-02-02 10:40:23 -0500

    Second Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,15 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Chelsea Penner
+ * Student Number: 991350908
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card myCard = new Card();
+       myCard.setSuit("Diamonds");
+       myCard.setValue(13);
     }
     
 }

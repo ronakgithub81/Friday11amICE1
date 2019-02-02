db41c60fbc14c514b5c946db8b681d156c717de1
Chelsea Penner <pennerch@sheridancollege.ca>
2019-02-02 10:59:01 -0500

    Fourth Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -12,6 +12,10 @@ public class CardTrick {
        Card myCard = new Card();
        myCard.setSuit("Diamonds");
        myCard.setValue(13);
+       
+       Card cardTwo = new Card();
+       cardTwo.setSuit("Hearts");
+       cardTwo.setValue(11);
         
        System.out.print("My card is the " + myCard.getValue() + "of " + myCard.getSuit() + "!");
     }
